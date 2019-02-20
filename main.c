674d89abf8b4d0fd45dd222bb8c10fb20f9f8632
Martin Androvich <martinandrovich@gmail.com>
2019-02-21 00:01:54 +0100

    Add tp_global

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
-#include "tm4c123gh6pm.h"
-#include "stdint.h"
-#include "src/button.h"
+#include <stdint.h>
 
-/**
- * main.c
- */
+#include "tm4c123gh6pm.h"
+#include "src/emp_type.h"
+#include "src/button.h"
+#include "src/timepoint.h"
+
+TIMEPOINT * tp_global;
+
 int main(void)
 {
 
+    // Initialize global timepoint instance
+    tp_global = new_TIMEPOINT(200);
 
 	return 0;
 }
