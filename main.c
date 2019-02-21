9bf58674c13e4551071a74be7004093843f63104
Martin Androvich <martinandrovich@gmail.com>
2019-02-21 15:44:30 +0100

    Added TIMEPOINT tests.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 /***************************** Include files *******************************/
 
 #include <stdint.h>
+#include <stdio.h>
 
 #include "tm4c123gh6pm.h"
 #include "src/emp_type.h"
@@ -28,7 +29,18 @@ int main(void)
 {
 
     // Initialize global timepoint instance
-    tp_global = new_TIMEPOINT(200);
+    tp_global = new_TIMEPOINT(SYSTEM);
+    tp_global->set_systick(tp_global, 200, ms);
 
+    // Initialize local timepoint instance
+    TIMEPOINT * tp_local = new_TIMEPOINT(NORMAL);
+
+    //tp_local->set_systick(tp_local, 200, ms); // should give runtime error
+
+    // Test
+    TIMEPOINT_copy(tp_local, tp_global);
+    int delta_value = TIMEPOINT_delta(tp_local, tp_global, ms);
+
+    printf("Test gives: %u.", delta_value);
 }
 
