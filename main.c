c704f0492fa1c0526d1385ebab68f6244a696df4
Martin Androvich <martinandrovich@gmail.com>
2019-02-27 14:19:48 +0100

    Test BUTTON

--- main.c
+++ main.c
@@ -24,21 +24,38 @@ void ISR_SYSTICK(void)
     // CLEAR INT FLAG
 }
 
+void mofo_dannyboy(INT64U duration_ms)
+{
+	printf("Daniel er en fucktard.");
+}
+
 /*******************************   Main   **********************************/
 int main(void)
 {
 
-    // init global timepoint instance
+    // enable IRQ
+	__enable_irq();
+
+	// init global timepoint instance
     tp_global = tp.new(SYSTEM);
-    tp.set_systick(tp_global, 200, ms);
-    tp.set_callback(tp_global, NULL);
+    //tp.set_systick(tp_global, 200, ms);
+
+    // create button SW1
+    BUTTON * btn_sw1 = btn.new(SW1);
+    btn.set_callback(btn_sw1, &mofo_dannyboy);
+
+    for(;;)
+    {
+    	btn.controller(btn_sw1);
+    }
+
 
     // init local timepoint instance
-    TIMEPOINT * tp_local = tp.new(NORMAL);
-    tp.set_value(tp_local, (INT64U[]){200, 0, 0, 2});
+    //TIMEPOINT * tp_local = tp.new(NORMAL);
+    //tp.set_value(tp_local, (INT64U[]){200, 0, 0, 2});
 
     // Test
-    int diff = tp.delta(tp_local, tp_global, ms);
-    printf("Test gives: %u.", diff);
+    //int diff = tp.delta(tp_local, tp_global, ms);
+    //printf("Test gives: %u.", diff);
 }
 
