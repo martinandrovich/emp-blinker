13c32673c51880ae6335769ad31966c5489e6416
Martin Androvich <martinandrovich@gmail.com>
2019-02-28 13:50:42 +0100

    Implement inline callbacks + cleaned code.

--- main.c
+++ main.c
@@ -13,15 +13,14 @@
 
 /*****************************    Defines    *******************************/
 
-#define SYSTICK_ms		1
+#define SYSTICK_DUR_MS		1
+#define LAMBDA(c_) ({ c_ _;}) // should be moved to driver
 
 /*****************************   Constants   *******************************/
 
 /*****************************   Variables   *******************************/
 
 TIMEPOINT * tp_global;
-LED * led_1;
-LED_CONTROLLER * ledc;
 
 /************************   Interrupt Handlers   ***************************/
 
@@ -30,60 +29,75 @@ void ISR_SYSTICK(void)
     tp.tick(tp_global);
 }
 
-void mofo_dannyboy(INT32S duration_ms)
-{
-	//printf("Daniel er en fucktard.");
-	__enable_irq();
-
-	if(duration_ms == -1)
-	{
-		//led.set_color(led_1, (RGB){0, 0, 1});
-		led.invert_colors(led_1);
-	}
-	else
-	{
-		led.toggle(led_1);
-	}
-
-	__disable_irq();
-}
-
-void mofo_pattyboy(INT32S duration_ms)
-{
-	led_ctrl.callback(ledc, duration_ms);
-}
-
 /*******************************   Main   **********************************/
+
 int main(void)
 {
+
+	/*****************************     Init     ****************************/
 
     // enable IRQ
 	__enable_irq();
 
-	// init global timepoint instance
+	// init global TIMEPOINT instance
     tp_global = tp.new(SYSTEM);
-    tp.set_systick(tp_global, SYSTICK_ms, ms);
+    tp.set_systick(tp_global, SYSTICK_DUR_MS, ms);
 
-    sys_tick_init(SYSTICK_ms);
+    // init SYSTICK
+    sys_tick_init(SYSTICK_DUR_MS);
 
-    // create LED_new
-    led_1 = led.new();
+    // init LED instance
+    LED * led_1 = led.new();
+
     led.set_color(led_1, (RGB){1, 0, 0});
     led.set_state(led_1, 1);
 
-    led.set_color(led_1, (RGB){0, 1, 0});
+    // init LED_CONTROLLER instance
+    LED_CONTROLLER* ledc_1 = led_ctrl.new();
 
-    // LED controller
-    ledc = led_ctrl.new();
+    // init BUTTON instance (SW1)
+    BUTTON * btn_sw1 = btn.new(SW1);
 
-    // create button SW1
-	BUTTON * btn_sw1 = btn.new(SW1);
-	btn.set_callback(btn_sw1, mofo_pattyboy);
+    // init callbacks (lambdas)
+
+    // method #1
+    //void sw_1_callback (INT32S duration_ms){ led_ctrl.callback(ledc_1, duration_ms); }
+	//btn.set_callback(btn_sw1, sw_1_callback);
+
+    // method #2
+	// btn.set_callback(btn_sw1,
+	// 	({
+	// 	 void sw_1_callback (INT32S duration_ms){ led_ctrl.callback(ledc_1, duration_ms); }
+	// 	 sw_1_callback;
+    // 	})
+	// );
+
+	// method #3 (same as 2, but using macro)
+	btn.set_callback(btn_sw1, LAMBDA(void _(INT32S duration_ms)
+		{
+			led_ctrl.callback(ledc_1, duration_ms);
+		}
+	));
+
+	/****************************    Main Loop      *************************/
 
     for(;;)
     {
     	btn.controller(btn_sw1);
-    	led_ctrl.controller(ledc, led_1);
+    	led_ctrl.controller(ledc_1, led_1);
     }
 
+    /****************************     Cleanup     **************************/
+
+    // disable IRQ
+    __disable_irq();
+
+    // free memory
+    tp.del(tp_global);
+    led.del(led_1);
+    btn.del(btn_sw1);
+    led_ctrl.del(ledc_1);
+
+    // exit
+    return 0;
 }
